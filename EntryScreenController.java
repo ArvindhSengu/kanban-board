9612240016ad1ddbb0647937c2ee5ebe53c7726d
ArvindhSengu <117862704+ArvindhSengu@users.noreply.github.com>
2023-01-11 07:58:16 -0500

    Create EntryScreenController.java

---
+++ EntryScreenController.java
@@ -0,0 +1,19 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField fileField;
+
+    @FXML
+    private Button importBtn;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button newBtn;
+
+}
