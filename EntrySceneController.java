9243a474504853e58a0f90f60cda4cf5aa6f2b0c
340886936 <340886936@gapps.yrdsb.ca>
2023-01-12 15:14:47 -0500

    changes to scenes

--- EntrySceneController.java
+++ EntrySceneController.java
@@ -2,7 +2,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class EntryScreenController {
+public class EntrySceneController {
 
     @FXML
     private TextField fileField;
