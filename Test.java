c84eeb999d504a6aeb5f6c0fe96a037287e887f0
AlanWXia <118770199+AlanWXia@users.noreply.github.com>
2023-01-17 22:10:35 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,11 @@
 class Test{
     public static void main(String[] args){
-        FReader fr = new FReader("C:\\Users\\alan3\\kanban_example.txt");
-        fr.readFile();
-        for(TaskDetails td : fr.getTaskDetails()){
+        TaskHandler th = new TaskHandler("C:\\Users\\alan3\\kanban_example.txt");
+        th.readFile();
+        th.addTask(new TaskDetails("learn driving", "get g2", "mid", "to do"));
+        th.deleteTask("exercise");
+        //th.sortTasks();
+        for(TaskDetails td : th.getTaskDetails()){
             System.out.println(td.toString());
         }
     }
