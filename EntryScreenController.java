cf26d8fc315946a1c15a45473525a1cc51425ed4
ArvindhSengu <117862704+ArvindhSengu@users.noreply.github.com>
2023-01-18 19:14:43 -0500

    Update EntryScreenController.java

--- EntryScreenController.java
+++ EntryScreenController.java
@@ -18,63 +18,17 @@ public class EntryScreenController {
     @FXML
     private Button newBtn;
 
-    private static String absolutePath = "";
-
-    /**
-     * Verifies if a file is on the users computer
-     * @param tField The text field
-     * @return The absolute path of the file
-     */
-    protected String verifyFile(TextField tField) {
-        boolean fileFound = false;
-        int count = 0;
-        while (true) {
-          if (fileFound) {
-            System.out.println("FILE FOUND! "); // Alan code goes here
-            break;
-          } 
-          else {
-            if(count>0){
-              System.out.println();
-              System.out.println("Invalid file!");
-            }
-            count++;
-            String fileName = "";
-            fileName = tField.getText();
-            String pathName = System.getProperty("user.dir");
-            fileFound = searchDir(pathName, fileName);
-          }
-        }
-        return absolutePath;
-      }
-     
-      /**
-       * Recursively searches directories for a file
-       * @param pathName Name of the file path
-       * @param fileName Name of the file
-       * @return Boolean based on if the file is found
-       */
-    protected boolean searchDir(String pathName, String fileName) {
-        File path = new File(pathName);
-        for (File file : path.listFiles()) {
-          if (file.isDirectory()) {
-            if (searchDir(file.getAbsolutePath(), fileName)) {
-              return true;
-            }
-            } else {
-            if (file.getName().equals(fileName)) {
-            absolutePath = file.getAbsolutePath();
-              return true;
-            }
-          }
-        }
-        return false;
-    }
-
     @FXML
     void importFile(ActionEvent event) {
-        verifyFile(fileField);
-        // Alan method
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Kanban Board");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("text file", "*.txt"));
+        try{
+            File selectedFile = fileChooser.showSaveDialog(stage);
+            filePath = selectedFile.getAbsolutePath;
+        }
+        catch(Exception ex){
+        }
     }
 
 }
