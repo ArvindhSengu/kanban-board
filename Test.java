6ff2995c5d7daec3a4d772f9b1f6531d5961b68e
AlanWXia <118770199+AlanWXia@users.noreply.github.com>
2023-01-20 21:07:36 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,8 +3,10 @@ class Test{
         TaskHandler th = new TaskHandler("C:\\Users\\alan3\\kanban_example.txt");
         th.readFile();
         th.addTask(new TaskDetails("learn driving", "get g2", "mid", "to do"));
-        th.deleteTask("exercise");
+        //th.deleteTask("exercise");
         th.rightArrow("learn driving");
+        th.updateTaskName("learn driving", "learn how to drive");
+        th.updateDescription("learn how to drive", "get ready for your g2 exam");
         th.sortTasks();
         for(TaskDetails td : th.getTaskDetails()){
             System.out.println(td.toString());
